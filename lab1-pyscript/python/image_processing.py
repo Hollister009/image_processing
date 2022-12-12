fa0a0ae4ec46737a2a1e2ce314f391f7d566841c
Hollister009 <reactive.andrew@yahoo.com.ua>
2022-12-12 18:37:29 +0200

    Save file processing

--- lab1-pyscript/python/image_processing.py
+++ lab1-pyscript/python/image_processing.py
@@ -1,5 +1,5 @@
-from js import document, window, console
-from pyodide.ffi import create_proxy
+from js import document, window, console, Object
+from pyodide.ffi import create_proxy, to_js
 import asyncio
 
 root = document.getElementById('root')
@@ -8,22 +8,44 @@ openButton = document.getElementById('openButton')
 saveButton = document.getElementById('saveButton')
 previewArea = document.getElementById('previewArea')
 
-is_save_disabbled = True
+target_file = None
 
 async def open_file(x):
   fileInput.click()
 
 async def process_file(event):
+  global target_file 
   file_list = event.target.files
-  first_item = file_list.item(0)
+  target_file = file_list.item(0)
 
   new_image = document.createElement('img')
-  new_image.setAttribute('src', window.URL.createObjectURL(first_item))
+  new_image.setAttribute('src', window.URL.createObjectURL(target_file))
   previewArea.innerHTML = ''
   previewArea.appendChild(new_image)
 
+async def save_file_old(file):
+  blob = window.URL.createObjectURL(file)
+  tag = document.createElement('a')
+  tag.href = blob
+  tag.download = file.name
+  tag.click()
+
 async def save_file(x):
-  pass
+  if not target_file:
+    return
+
+  try:
+    options = {
+      "startIn": "downloads",
+      "suggestedName": target_file.name
+    }
+    fileHandle = await window.showSaveFilePicker(Object.fromEntries(to_js(options)))
+    file = await fileHandle.createWritable()
+    await file.write(target_file)
+    await file.close()
+  except Exception as e:
+    console.log('Exception: ' + str(e))
+    await save_file_old(target_file)
 
 def main():
   # Create a Python proxy for the callback function
